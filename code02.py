322f5229696f2426ccc8deceb386efea22d95b52
Gaurav Bansal <gauravmohanbansal@gmail.com>
2021-05-07 00:40:21 +0800

    Create code02.py

---
+++ code02.py
@@ -0,0 +1,52 @@
+import argparse
+
+from google.cloud import language_v1
+
+
+
+def print_result(annotations):
+    score = annotations.document_sentiment.score
+    magnitude = annotations.document_sentiment.magnitude
+
+    for index, sentence in enumerate(annotations.sentences):
+        sentence_sentiment = sentence.sentiment.score
+        print(
+            "Sentence {} has a sentiment score of {}".format(index, sentence_sentiment)
+        )
+
+    print(
+        "Overall Sentiment: score of {} with magnitude of {}".format(score, magnitude)
+    )
+    return 0
+
+
+
+
+def analyze(movie_review_filename):
+    """Run a sentiment analysis request on text within a passed filename."""
+    client = language_v1.LanguageServiceClient()
+
+    with open(movie_review_filename, "r") as review_file:
+        # Instantiates a plain text document.
+        content = review_file.read()
+
+    document = language_v1.Document(content=content, type_=language_v1.Document.Type.PLAIN_TEXT)
+    annotations = client.analyze_sentiment(request={'document': document})
+
+    # Print the results
+    print_result(annotations)
+
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+    parser.add_argument(
+        "movie_review_filename",
+        help="The filename of the movie review you'd like to analyze.",
+    )
+    args = parser.parse_args()
+
+    analyze(args.movie_review_filename)
